56ec3251d08c76acba2519dc2d30a5e408d4d4eb
Balti2003 <baltasarlomello@live.com>
2025-05-23 22:11:46 +0000

    Probando algunos tests

---
+++ profiles/tests/test_models.py
@@ -0,0 +1,19 @@
+from django.test import TestCase
+from profiles.models import UserProfile, Follow
+from django.contrib.auth.models import User
+
+class UserProfileModelTest(TestCase):
+    
+    def setUp(self):
+        print("setUp")
+        
+        self.user1 = User.objects.create_user(username="user1", password="1234")
+        self.user2 = User.objects.create_user(username="user2", password="1234")
+        self.profile1 = UserProfile.objects.create(user=self.user1, bio="hola", birth_date="2000-01-01")
+        self.profile2 = UserProfile.objects.create(user=self.user2, bio="chau", birth_date="2000-02-02")
+        
+    def test_follow_user(self):
+        self.profile1.follow(self.profile2)
+        
+        self.assertTrue(Follow.objects.filter(follower=self.profile1, following=self.profile2).exists())
+        
